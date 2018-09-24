66e7d6a070d00c197357cc95ba33fbc6ff646422
eduardo18delta <eduardo18delta@gmail.com>
2018-09-24 19:53:07 -0300

    Modificações

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 
-require_once 'Pessoa.php';
-require_once 'Pessoa2.php';
 require_once 'vendor/autoload.php';
-
-
-
-$pessoa = new \Projeto\Pessoa('Eduardo' , 35, '1.85');
-
-$pessoa2 = new  Pessoa('Eduardo' , 35, '1.85');
-
-$pessoa->andar();
-$pessoa2->andar();
-
-echo $pessoa->identificacao();
-
-
-
-
-
 
 
 
