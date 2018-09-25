b912311c355258b103f18003460a100fa38c4f4f
eduardo18delta <eduardo18delta@gmail.com>
2018-09-25 15:24:48 -0300

    teste de error

--- index.php
+++ index.php
@@ -41,3 +41,47 @@
 
 </body>
 </html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Gerador de Qr Code</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+
+<div class="jumbotron">
+  <h1>Gerador de Qrcode</h1>
+</div>
+
+<form action="qrcode.php" method="GET">
+
+<div class="form-group">	
+<label>Insira um texto:</label>
+<input class="form-control" type="text" name="qrcode" required="">
+</div>
+
+<div class="form-group">	
+<label>Insira uma data:</label>
+<input class="form-control" type="text" name="dtnasc" required="">
+</div>
+
+<div class="form-group">	
+<label>Insira um email:</label>	
+<input class="form-control" type="text" name="email" required="">
+</div>
+
+
+<div class="form-group">	
+<button class="btn btn-success" type="submit">Enviar</button>
+</div>
+
+</form>
+
+</div>
+
+</body>
